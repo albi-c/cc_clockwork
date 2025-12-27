15f8ccf806df68984df3a00d78b2876a624c66b8
albi-c <albert.csontos@gmail.com>
2025-12-27 09:21:38 +0100

    fix phys bearing control

--- src/main/java/me/tabin/cc_cw/peripherals/PhysBearingPeripheral.java
+++ src/main/java/me/tabin/cc_cw/peripherals/PhysBearingPeripheral.java
@@ -1,6 +1,5 @@
 package me.tabin.cc_cw.peripherals;
 
-import com.simibubi.create.foundation.blockEntity.behaviour.scrollValue.ScrollOptionBehaviour;
 import dan200.computercraft.api.lua.LuaException;
 import dan200.computercraft.api.lua.LuaFunction;
 import dan200.computercraft.api.peripheral.GenericPeripheral;
@@ -8,7 +7,6 @@ import me.tabin.cc_cw.ComputercraftClockwork;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import org.jspecify.annotations.NonNull;
 import org.valkyrienskies.clockwork.content.contraptions.phys.bearing.PhysBearingBlockEntity;
-import org.valkyrienskies.clockwork.platform.api.ContraptionController;
 
 import java.util.Objects;
 
@@ -18,44 +16,28 @@ public class PhysBearingPeripheral implements GenericPeripheral {
         return ComputercraftClockwork.resourceLocation("phys_bearing").toString();
     }
 
-    private @NonNull ScrollOptionBehaviour<ContraptionController.LockedMode> getMovementMode(@NonNull PhysBearingBlockEntity be) {
-        return Objects.requireNonNull(be.getMovementMode());
-    }
-
-    public @LuaFunction void assemble(@NonNull PhysBearingBlockEntity be) {
-        be.setAssembleNextTick(true);
-    }
-    public @LuaFunction void disassemble(@NonNull PhysBearingBlockEntity be) {
-        be.disassemble();
-    }
     public @LuaFunction boolean isAssembled(@NonNull PhysBearingBlockEntity be) {
         return be.isRunning();
     }
 
-    public @LuaFunction void setLockedMode(@NonNull PhysBearingBlockEntity be) {
-        getMovementMode(be).setValue(ContraptionController.LockedMode.LOCKED.ordinal());
+    public @LuaFunction void lock(@NonNull PhysBearingBlockEntity be) {
+        be.setStopTargetAngleChange(true);
     }
-    public @LuaFunction void setUnlockedMode(@NonNull PhysBearingBlockEntity be) {
-        getMovementMode(be).setValue(ContraptionController.LockedMode.UNLOCKED.ordinal());
+    public @LuaFunction void unlock(@NonNull PhysBearingBlockEntity be) {
+        be.setStopTargetAngleChange(false);
     }
     public @LuaFunction void setAngle(@NonNull PhysBearingBlockEntity be, double angle) throws LuaException {
         if (be.getStopTargetAngleChange()) {
             be.setAngle((float) angle);
         } else {
-            throw new LuaException("Can't be changed until target angle change is stopped");
+            throw new LuaException("Angle can't be changed unless bearing is locked");
         }
     }
 
     public @LuaFunction boolean isBeingDisassembled(@NonNull PhysBearingBlockEntity be) {
         return be.getDisassembleWhenPossible();
     }
-    public @LuaFunction boolean isActive(@NonNull PhysBearingBlockEntity be) {
-        return be.isRunning();
-    }
-    public @LuaFunction boolean isInLockedMode(@NonNull PhysBearingBlockEntity be) {
-        return getMovementMode(be).get() == ContraptionController.LockedMode.LOCKED;
-    }
-    public @LuaFunction boolean targetAngleIsFrozen(@NonNull PhysBearingBlockEntity be) {
+    public @LuaFunction boolean isLocked(@NonNull PhysBearingBlockEntity be) {
         return be.getStopTargetAngleChange();
     }
 
@@ -66,15 +48,15 @@ public class PhysBearingPeripheral implements GenericPeripheral {
         return be.getTargetAngle();
     }
     public @LuaFunction double getActualAngle(@NonNull PhysBearingBlockEntity be) {
-        return Objects.requireNonNull(be.getActualAngle());
+        return Objects.requireNonNullElse(be.getActualAngle(), 0.0);
     }
-    public @LuaFunction float getTargetAngleChangeSpeed(@NonNull PhysBearingBlockEntity be) {
+    public @LuaFunction float getAngularSpeed(@NonNull PhysBearingBlockEntity be) {
         return be.getActualAngularSpeed();
     }
     public @LuaFunction float getRPM(@NonNull PhysBearingBlockEntity be) {
         return be.getSpeed();
     }
-    public @LuaFunction @NonNull String getFacingDirection(@NonNull PhysBearingBlockEntity be) {
+    public @LuaFunction @NonNull String getDirection(@NonNull PhysBearingBlockEntity be) {
         return be.getBlockState().getValue(BlockStateProperties.FACING).getName();
     }
 }
